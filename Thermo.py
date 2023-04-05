a063ee2911e8b5b017b5c29d1d743296cb793365
LucasJCodes <lucas.jones.home@gmail.com>
2023-04-05 18:35:31 -0500

    Testing function

--- Thermo.py
+++ Thermo.py
@@ -16,8 +16,10 @@ Cp = 1005 # J/kg K
 
 #Question 1
 
-def findTemps(p0, alpha0):
+def findTemp(p0, alpha0):
     temperature = (p0 * alpha0) / Rd
     
     return temperature
 
+
+print("temperature 1: {} temperature 2: {}".format(findTemp(p0, alpha0), findTemp((2 * p0), alpha0)))
